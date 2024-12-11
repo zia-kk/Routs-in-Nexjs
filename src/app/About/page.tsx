3de0210563b8f393ecce36ffa581e174932cf502
Zia Ur Rehman <162309768+zia-kk@users.noreply.github.com>
2024-12-10 18:52:15 -0800

    Routs Working in next js

---
+++ src/app/About/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='bg-yellow-400 text-red-600 h-80 flex justify-center items-center'>
+        <h1 className='text-3xl font-bold '>This is about Page</h1>
+    </div>
+  )
+}
+
+export default Page
